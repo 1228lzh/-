4155342137d8a835db3aa3f7919f3ee718b2e358
ZH <1412158844@qq.com>
2022-11-14 21:25:35 +0800

    自定义后端简单伪登陆

---
+++ common/common_util/src/main/java/com/zh/common/result/ResultCode.java
@@ -0,0 +1,11 @@
+package com.zh.common.result;
+
+/**
+ * @Author: ZH
+ * @Date: 2022/11/8 20:39
+ */
+public class ResultCode {
+    public  static  Integer SUCCESS = 200;
+    public  static  Integer ERROR = 201;
+
+}
